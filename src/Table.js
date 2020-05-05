365e41bd7a18c5382fc53b7905892988d068f10d
wrunion <winterrunion@gmail.com>
2020-05-05 11:13:47 -0700

    Add deleteCharacter function and button.

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const TableHeader = () => {
   return (
@@ -17,23 +17,24 @@ const TableBody = (props) => {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
   })
-
   return <tbody>{rows}</tbody>
 }
 
-class Table extends Component {
-  render() {
-    const { characterData } = this.props
+const Table = props => {
+  const { characterData, removeCharacter } = props;
+
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={characterData} />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
       </table>
     );
-  }
 }
 
 export default Table;
